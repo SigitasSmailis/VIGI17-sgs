43b00f74bfc595c9fe12241b8afa08b22808b39e
Sigitas Smailis <tasliunasgmail.com>
2022-04-26 19:54:35 +0300

    VIGI 1 paskaita

---
+++ JS/VIGI JS 1/script.js
@@ -0,0 +1,103 @@
+console.log ("Mano vardas nuostabus")
+var myName = 78;
+console.log(myName);
+
+//ujhtyjfgkjgh
+
+// vbjnfhhd
+// hgxjnm
+// hdjhfjjjgfxjmgh
+
+// lorem20
+// gxnxgjgh
+// xgjhnx
+
+
+console.log ("first");
+
+console.log('first');
+
+let manovardas;
+
+let manoVardas = 'Nuostabusis';
+console.log (manoVardas);
+
+var as=('kiekis');
+console.log(as);
+var as=4;
+console.log(as);
+
+const tikras = 7;
+
+console.log(tikras);
+
+let tikra = 7;
+tikra='pieva';
+console.log(tikra);
+
+let array = [
+    'tekstas', 5, '5',true
+]
+console.log(array);
+
+let object ={
+    name: 'kazys',
+    fklasd: '55',
+    artikrai: true,
+
+}
+console.log(object);
+
+let d=4;
+console.log (d++)
+console.log (d)
+console.log (d % 2)
+
+// alert('d-verte: ' + d);
+
+
+// const namE = prompt('parasykite varda');
+// console.log(namE +20);
+let u=5;
+
+console.log(5 == u);
+
+console.log(5 === u);
+console.log(5 == '5');
+
+// let value = prompt('reiksme');
+// console.log(value === 'vardas');
+// console.log(value === '5');
+
+// const value1 = +prompt('reiksmele');
+
+// console.log(value1 > 5);
+// console.log(value1 >= 5);
+
+console.log (2===2 && 'kurmis' === 'kurmis')
+console.log (2===3 || 'kurmis' === 'kurmis')
+console.log (2===3 || 'kurmis' === 'kurmis' && 5===5)
+
+
+// if (3===5){
+//     alert('tai yra teisybe')
+// } else {console.log ('tai klaida')}
+
+
+const promtValue = +prompt('iveskite skaiciu')
+// if (promtValue===5){
+//     console.log('tai yra teisybe')
+// } else {console.log ('tai klaida')}
+
+if (promtValue===15){
+    console.log('tai yra teisybe gavome 15')
+} else if (promtValue===20){
+    console.log('tai yra teisybe gavome 20')
+} else if (promtValue===40){
+    console.log('tai yra teisybe gavome 40')}
+else {
+    console.log ('tai klaida')}
+
+
+ console.log (5!==5) 
+  console.log (4.5  !==5)   //ar tai nera lygu
